6cd99a3a3a6c14dc2f027d5029958f53d2549470
kirabiyakasa <icantbestoppedfools@gmail.com>
2020-10-25 23:41:56 -0400

    Add postgres gem

--- db/migrate/20201021102803_create_comments.rb
+++ db/migrate/20201021102803_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.string :author_name
+      t.text :author_name
       t.text :body
       t.references :article, foreign_key: true
 
